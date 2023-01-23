c078d98bbf0e99ac8078d32948a3a8e02d863d87
Emre Guneyler <eguneys@gmail.com>
2023-01-23 21:58:49 +0300

    deploy

---
+++ linkstuff.sh
@@ -0,0 +1,17 @@
+git clone https://github.com/eguneys/blah
+cd blah
+pnpm install
+pnpm build
+pnpm link --dir ./ --global
+cd ..
+git clone https://github.com/eguneys/lsolitaire
+cd lsolitaire
+pnpm install
+pnpm validate
+pnpm link --dir ./ --global
+cd ..
+git clone https://github.com/eguneys/aset
+cd aset
+pnpm install
+pnpm bundle
+pnpm link --dir ./ --global
