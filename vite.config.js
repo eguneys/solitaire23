fba65cfee3b47e8c6dc48801f0fcc227080844d6
eguneys <eguneys@gmail.com>
2024-01-04 02:03:56 +0300

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import glsl from 'vite-plugin-glsl'
 import { defineConfig } from 'vite'
 
+import { VitePWA } from 'vite-plugin-pwa'
+
 export default defineConfig({
   base: './',
-  plugins: [glsl()]
+  plugins: [glsl(),
+  VitePWA({ 
+    manifest: { 
+name: 'Sofo Solitaire',
+short_name: "Sofo",
+start_url: ".",
+display: "standalone",
+background_color: "#315594",
+description: "Solitaire 3 Card Games",
+icons: [{
+    src: "images/touch/homescreen192.png",
+    sizes: "192x192",
+    type: "image/png"
+  }]
+  }, injectRegister: 'inline', registerType: 'autoUpdate'})]
 })
